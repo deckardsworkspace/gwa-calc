ee12a34f6e15b511d7f5bc1dd630d762ebb2b053
Jared Dantis <jareddantis@gmail.com>
2024-04-21 19:48:53 +0800

    src: Generate client

--- Makefile
+++ Makefile
@@ -1,8 +1,16 @@
-.PHONY: install dev build precommit
+.PHONY: install client dev build precommit
 
 install:
 	npm install
 	pre-commit install
+
+SCHEMA_PATH ?= ../backend/openapi.json
+client:
+	@npx @openapitools/openapi-generator-cli \
+		generate -i $(SCHEMA_PATH) \
+		-g typescript-fetch \
+		-o src/api && \
+		find src/api -type f -name '*.ts' | xargs pre-commit run --files
 
 dev:
 	npm run dev
---
+++ src/api/models/index.ts
@@ -0,0 +1,20 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './CourseCreateRequest';
+export * from './CourseMultipleReadRequest';
+export * from './CourseMultipleResponse';
+export * from './CoursePydantic';
+export * from './CourseUpdateRequest';
+export * from './GradesUpdateRequest';
+export * from './GradingSystem';
+export * from './HTTPValidationError';
+export * from './LoginRequest';
+export * from './LoginResponse';
+export * from './SignupRequest';
+export * from './TermCreateRequest';
+export * from './TermGradesPydantic';
+export * from './TermPydantic';
+export * from './TermUpdateRequest';
+export * from './UserPydantic';
+export * from './ValidationError';
+export * from './ValidationErrorLocInner';
