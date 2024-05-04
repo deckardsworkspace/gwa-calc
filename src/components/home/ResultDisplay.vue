3928d94270fe186ed82b516a121facdaf2a047d5
Jared Dantis <jareddantis@gmail.com>
2024-05-04 13:45:10 +0800

    ResultDisplay: Remove rounded corners on scroll

--- src/components/home/ResultDisplay.vue
+++ src/components/home/ResultDisplay.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { IconBackspace, IconCheck } from '@iconify-prerendered/vue-material-symbols';
-import { computed } from 'vue';
+import { computed, onMounted, ref } from 'vue';
 import { useGradesStore } from '@/stores/grades';
 import { useToastStore } from '@/stores/toast';
 import ActionButton from '../common/ActionButton.vue';
@@ -16,16 +16,43 @@ const clearResult = () => {
   store.resetGrades();
   toastStore.showToast('Grades cleared', IconCheck());
 };
+
+// Change the styles of the result display when it snaps to top on scroll
+const isStuck = ref(false);
+const dynamicStyles = computed(() => {
+  if (isStuck.value) {
+    return 'from-orange-200';
+  }
+
+  return 'rounded-3xl inset-shadow from-orange-100';
+});
+onMounted(() => {
+  const resultDisplay = document.getElementById('result-display');
+  if (!resultDisplay) {
+    return;
+  }
+
+  const observer = new IntersectionObserver(
+    ([entry]) => {
+      isStuck.value = entry.intersectionRatio < 1;
+    },
+    { threshold: [1] }
+  );
+
+  observer.observe(resultDisplay);
+});
 </script>
 
 <template>
-  <div
-    class="inset-shadow w-full p-8 mb-12 sticky top-0 z-10 flex justify-between items-center bg-gradient-to-b from-orange-100 to-orange-300 rounded-3xl"
-  >
-    <h1 class="numeric font-light text-6xl text-orange-800">{{ result }}</h1>
+  <div id="result-display" class="sticky z-10 mb-12 w-full">
+    <div
+      :class="`${dynamicStyles} transition-all duration-300 flex justify-between p-8 items-center bg-gradient-to-b to-orange-300`"
+    >
+      <h1 class="numeric font-light text-6xl text-orange-800">{{ result }}</h1>
 
-    <div class="flex flex-row-reverse gap-4">
-      <ActionButton :icon="IconBackspace" color="orange" @click="clearResult" />
+      <div class="flex flex-row-reverse gap-4">
+        <ActionButton :icon="IconBackspace" color="orange" @click="clearResult" />
+      </div>
     </div>
   </div>
 </template>
@@ -33,5 +60,9 @@ const clearResult = () => {
 <style scoped>
 .inset-shadow {
   box-shadow: 0px 7px 13.4px 1px rgba(194, 65, 12, 0.2) inset;
+}
+#result-display {
+  top: -1px;
+  padding-top: 1px;
 }
 </style>
